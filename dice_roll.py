f6ef03c8febb80b5fd71a45ec245491e01fb6031
JontePonte <john_nordstrand@hotmail.com>
2022-08-01 20:31:45 +0200

    created dice roll function

---
+++ dice_roll.py
@@ -0,0 +1,8 @@
+''' Bla bla '''
+
+import random
+
+
+def dice_roll():
+    ''' Return random dice roll '''
+    return random.randint(1,6)
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 
-print("Hello World")
+from dice_roll import dice_roll
+
+
+print(dice_roll())
