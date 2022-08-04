1760e304dfe162aaa5a9bb81857f845b1a846aa3
JontePonte <john_nordstrand@hotmail.com>
2022-08-04 19:41:10 +0200

    Added multiple calculations from 0 to TARGET_SUM

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ Runs dice game multiple times and create statistics for the results
 from dice_game import dice_game
 
 NUMBER_OF_PLAYERS = 2
-TARGET_SUM = 10
+TARGET_SUM = 51
 NUMBER_OF_ROUNDS = 10000
 
-player_wins = [0]*NUMBER_OF_PLAYERS
+for target_sum in range(TARGET_SUM):
+    player_wins = [0]*NUMBER_OF_PLAYERS
 
+    for _ in range(NUMBER_OF_ROUNDS):
+        round_winner = dice_game(NUMBER_OF_PLAYERS, target_sum)
 
-for _ in range(NUMBER_OF_ROUNDS):
-    round_winner = dice_game(NUMBER_OF_PLAYERS,TARGET_SUM)
+        for (player_id, _) in enumerate(player_wins):
+            if round_winner == player_id:
+                player_wins[player_id] += 1
 
-    for (player_id, _) in enumerate(player_wins):
-        if round_winner == player_id:
-            player_wins[player_id] += 1
+    win_fractions = []
+    _ = [win_fractions.append((i/NUMBER_OF_ROUNDS)) for i in player_wins]
 
-win_fractions = []
-_ = [win_fractions.append((i/NUMBER_OF_ROUNDS)) for i in player_wins]
-
-print(win_fractions)
+    print(target_sum, win_fractions)
